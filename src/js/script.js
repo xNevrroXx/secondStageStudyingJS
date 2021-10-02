4d76bebcc1d677c26b122a850fa3383282a0653d
Vladimir <govorov2001gv@gmail.com>
2021-10-03 00:49:19 +0300

    finished second practice with DOM-elements

--- src/js/script.js
+++ src/js/script.js
@@ -1,17 +1,3 @@
-/* Задания на урок:
-
-1) Удалить все рекламные блоки со страницы (правая часть сайта)
-
-2) Изменить жанр фильма, поменять "комедия" на "драма"
-
-3) Изменить задний фон постера с фильмом на изображение "bg.jpg". Оно лежит в папке img.
-Реализовать только при помощи JS
-
-4) Список фильмов на странице сформировать на основании данных из этого JS файла.
-Отсортировать их по алфавиту 
-
-5) Добавить нумерацию выведенных фильмов */
-
 'use strict';
 
 const movieDB = {
@@ -24,3 +10,89 @@ const movieDB = {
     ]
 };
 
+const adv = document.querySelectorAll('.promo__adv img'),
+      promoBg = document.querySelector('.promo__bg'),
+      genre = promoBg.querySelector('.promo__genre'),
+      filmList = document.querySelector('.promo__interactive-list'),
+      btnAddFilm = document.querySelector('form.add > button'),
+      inputedFilmInForm = document.getElementById('watchedFilm');
+
+let deleteFilmFromListBTNS = document.querySelectorAll('.delete');
+
+adv.forEach(item => {
+    item.remove();
+});
+
+genre.textContent = 'драма';
+
+promoBg.style.backgroundImage = "url('img/bg.jpg')";
+
+addFilm();
+initializeDeleteFilmFunction();
+
+
+//functions
+function addFilm() {
+    btnAddFilm.addEventListener('click', (e) => {
+        if(inputedFilmInForm.value != "") {  
+            if(document.querySelector("form.add input:nth-of-type(n+2)").checked) {
+                console.log("Добавляем любимый фильм!");
+            } 
+            e.preventDefault();
+            movieDB.movies.push(firstSymbolToUpper(inputedFilmInForm.value, 0));
+            inputedFilmInForm.value = "";
+            updateFilmList();
+        }
+    });
+}
+
+function noMoreXSymbols(x, str) { //x - максимальное кол-во выводимых символов
+    if(str.length > 21) {
+        str = str.slice(0, x+1) + "...";
+
+        for (let i = str.length; i > 0; i--) { //дополнительно обрезает на последнем пробеле
+            if (str[i] == " ") {
+                str = str.slice(0, i+1) + "...";
+                break;
+            }
+        }
+    }
+    return str;
+}
+
+function firstSymbolToUpper(str, numFirstSymbol) {
+    str = str[numFirstSymbol].toUpperCase() + str.toLowerCase().slice(numFirstSymbol+1);
+    str = str.replace(/(\r\n|\n|\r)/gm,"");
+    str = str.trim();
+    return str;
+}
+
+function updateFilmList() {//помещается обновленный список фильмов на страницу
+    movieDB.movies.sort(); 
+    filmList.innerHTML = "";
+    movieDB.movies.forEach( (film, i) => { 
+        filmList.innerHTML += `
+            <li class="promo__interactive-item">${i+1}) ${noMoreXSymbols(21, film)}
+                <div class="delete"></div>
+            </li>
+        `;
+    });
+    deleteFilmFromListBTNS = document.querySelectorAll('.delete');
+    initializeDeleteFilmFunction();
+}
+
+function initializeDeleteFilmFunction() {
+    deleteFilmFromListBTNS.forEach ( item => {
+        item.addEventListener("click", () => {
+            const deletedElement = firstSymbolToUpper(item.parentElement.textContent, 3);
+
+            movieDB.movies.forEach( (film, i) => { //удаляет элемент из массива
+                if(firstSymbolToUpper(film, 0) == firstSymbolToUpper(deletedElement, 0)) {
+                    movieDB.movies.splice(i,1);
+                }
+            });
+            item.parentElement.remove(); //удаляет элемент со страницы
+            updateFilmList();
+        });
+    });
+}
